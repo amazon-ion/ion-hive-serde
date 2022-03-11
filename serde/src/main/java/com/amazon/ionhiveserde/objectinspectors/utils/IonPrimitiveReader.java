65495f63d19e0975b43285021aa9ba16f17b69f1
Jacob Stein <jacobstn@amazon.com>
2022-03-11 12:18:46 -0800

    Add javadoc for IonPrimitiveReader, call appropriate .xxxValue function in reader

--- serde/src/main/java/com/amazon/ionhiveserde/objectinspectors/utils/IonPrimitiveReader.java
+++ serde/src/main/java/com/amazon/ionhiveserde/objectinspectors/utils/IonPrimitiveReader.java
@@ -16,11 +16,9 @@
 package com.amazon.ionhiveserde.objectinspectors.utils;
 
 import com.amazon.ion.IonReader;
-import com.amazon.ion.IonSystem;
-import com.amazon.ion.IonTimestamp;
 import com.amazon.ion.IonType;
 import com.amazon.ion.Timestamp;
-import com.amazon.ion.system.IonSystemBuilder;
+import com.amazon.ion.system.IonReaderBuilder;
 import com.google.common.collect.ImmutableSet;
 
 import java.math.BigDecimal;
@@ -28,62 +26,126 @@ import java.util.Date;
 import java.util.Set;
 
 public class IonPrimitiveReader {
-    private static final IonSystem SYSTEM = IonSystemBuilder.standard().build();
+
+    private static final IonReaderBuilder READER_BUILDER = IonReaderBuilder.standard().immutable();
 
     // Restricted to boolean ion types
+    /**
+     * Convert a string containing a single serialized IonValue to a boolean.
+     * @param ionPrimitive - String with serialized Ion value.
+     * @return boolean value.
+     */
     public static boolean booleanValue(final String ionPrimitive) {
         return buildSingleReader(ionPrimitive, CastType.BOOLEAN).booleanValue();
     }
 
     // Restricted to numeric ion types
+    /**
+     * Convert a string containing a single serialized IonValue to a byte.
+     * @param ionPrimitive - String with serialized Ion value.
+     * @return byte value.
+     */
     public static byte byteValue(final String ionPrimitive) {
         return (byte) buildSingleReader(ionPrimitive, CastType.NUMERIC).longValue();
     }
 
+    /**
+     * Convert a string containing a single serialized IonValue to a BigDecimal.
+     * @param ionPrimitive - String with serialized Ion value.
+     * @return BigDecimal value.
+     */
     public static BigDecimal decimalValue(final String ionPrimitive) {
         return buildSingleReader(ionPrimitive, CastType.NUMERIC).bigDecimalValue();
     }
 
+    /**
+     * Convert a string containing a single serialized IonValue to a double.
+     * @param ionPrimitive - String with serialized Ion value.
+     * @return double value.
+     */
     public static double doubleValue(final String ionPrimitive) {
         return buildSingleReader(ionPrimitive, CastType.NUMERIC).doubleValue();
     }
 
+    /**
+     * Convert a string containing a single serialized IonValue to a float.
+     * @param ionPrimitive - String with serialized Ion value.
+     * @return float value.
+     */
     public static float floatValue(final String ionPrimitive) {
-        return (float) buildSingleReader(ionPrimitive, CastType.NUMERIC).doubleValue();
+        return (float) buildSingleReader(ionPrimitive, CastType.FLOAT).doubleValue();
     }
 
+    /**
+     * Convert a string containing a single serialized IonValue to an int.
+     * @param ionPrimitive - - String with serialized Ion value.
+     * @return int value.
+     */
     public static int intValue(final String ionPrimitive) {
         return (int) buildSingleReader(ionPrimitive, CastType.NUMERIC).longValue();
     }
 
+    /**
+     * Convert a string containing a single serialized IonValue to a long.
+     * @param ionPrimitive - String with serialized Ion value.
+     * @return long value.
+     */
     public static long longValue(final String ionPrimitive) {
         return buildSingleReader(ionPrimitive, CastType.NUMERIC).longValue();
     }
 
+    /**
+     * Convert a string containing a single serialized IonValue to a short.
+     * @param ionPrimitive - String with serialized Ion value.
+     * @return short value.
+     */
     public static short shortValue(final String ionPrimitive) {
         return (short) buildSingleReader(ionPrimitive, CastType.NUMERIC).longValue();
     }
 
     // Restricted to temporal ion types
+
+    /**
+     * Convert a string containing a single serialized IonValue to an Ion date.
+     * @param ionPrimitive - String with serialized Ion value.
+     * @return Ion date value.
+     */
     public static Date dateValue(final String ionPrimitive) {
         return buildSingleReader(ionPrimitive, CastType.TEMPORAL).dateValue();
     }
 
+    /**
+     * Convert a string containing a single serialized IonValue to an Ion timestamp.
+     * @param ionPrimitive - String with serialized Ion value.
+     * @return Ion timestamp value.
+     */
     public static Timestamp timestampValue(final String ionPrimitive) {
         Timestamp ts = buildSingleReader(ionPrimitive, CastType.TEMPORAL).timestampValue();
         return ts;
     }
 
     // No restrictions on ion type
+    /**
+     * Reads a single string containing a serialized IonValue as a string.
+     * @param ionPrimitive - String with serialized Ion value.
+     * @return String value containing text representation of an IonValue.
+     */
     public static String stringValue(final String ionPrimitive) {
-        return buildSingleReader(ionPrimitive, CastType.ALL).stringValue();
+        IonReader reader = READER_BUILDER.build(ionPrimitive);
+        IonType type = reader.next();
+        validateReader(ionPrimitive, type, CastType.STRING);
+        return toStringValue(reader, type, ionPrimitive);
     }
 
-
-
     private static IonReader buildSingleReader(final String ionPrimitive, final CastType targetType) {
-        IonReader reader = SYSTEM.newReader(ionPrimitive);
+        IonReader reader = READER_BUILDER.build(ionPrimitive);
         IonType type = reader.next();
+        validateReader(ionPrimitive, type, targetType);
+        return reader;
+    }
+
+    private static void validateReader(final String ionPrimitive, final IonType type, final CastType targetType) {
+
         if (!targetType.canCast(type)) {
             throw new IllegalArgumentException(
                     String.format("Unable to cast value %s of type %s as %s type.",
@@ -91,27 +153,54 @@ public class IonPrimitiveReader {
                             type.name(),
                             targetType.toString().toLowerCase()));
         }
-        return reader;
+    }
+
+    private static String toStringValue(final IonReader reader, final IonType type, final String ionPrimitive) {
+        String stringValue;
+        switch (type) {
+            case BOOL:
+                stringValue = String.valueOf(reader.booleanValue());
+                break;
+            case DECIMAL:
+                stringValue = reader.decimalValue().toString();
+                break;
+            case FLOAT:
+                stringValue = String.valueOf(reader.doubleValue());
+                break;
+            case INT:
+                stringValue = String.valueOf(reader.intValue());
+                break;
+            case TIMESTAMP:
+                stringValue = reader.timestampValue().toString();
+                break;
+            default:
+                stringValue = buildSingleReader(ionPrimitive, CastType.STRING).stringValue();
+        }
+        return stringValue;
     }
 
     private enum CastType {
-        ALL,
         BOOLEAN,
+        FLOAT,
         NUMERIC,
+        STRING,
         TEMPORAL;
 
         private static Set<IonType> NUMERIC_TYPE = ImmutableSet.of(IonType.INT, IonType.DECIMAL, IonType.FLOAT);
+        private static Set<IonType> FLOAT_TYPE = ImmutableSet.of(IonType.DECIMAL, IonType.FLOAT);
 
         public boolean canCast(final IonType type) {
             switch (this) {
-                case ALL:
-                    return true;
                 case BOOLEAN:
                     return type == IonType.BOOL;
-                case TEMPORAL:
-                    return type == IonType.TIMESTAMP;
+                case FLOAT:
+                    return FLOAT_TYPE.contains(type);
                 case NUMERIC:
                     return NUMERIC_TYPE.contains(type);
+                case STRING:
+                    return true;
+                case TEMPORAL:
+                    return type == IonType.TIMESTAMP;
                 default:
                     return false;
             }
