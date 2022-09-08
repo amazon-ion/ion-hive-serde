823f67c8f54732586ef9e80ba01dcc1aa98214c9
Eric Chen <cheqianh@amazon.com>
2022-09-08 16:00:06 -0700

    handle Null for structToMap.

--- serde/src/main/java/com/amazon/ionhiveserde/objectinspectors/IonStructToMapObjectInspector.java
+++ serde/src/main/java/com/amazon/ionhiveserde/objectinspectors/IonStructToMapObjectInspector.java
@@ -22,6 +22,9 @@ import com.amazon.ion.IonSymbol;
 import com.amazon.ion.IonValue;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 import org.apache.hadoop.hive.serde2.objectinspector.MapObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
@@ -62,9 +65,8 @@ public class IonStructToMapObjectInspector implements MapObjectInspector {
 
         final IonStruct struct = (IonStruct) data;
         final IonSymbol symbol = (IonSymbol) key;
-        IonValue v = struct.get(symbol.stringValue());
 
-        return IonUtil.isIonNull(v) ? null : v;
+        return IonUtil.handleNull(struct.get(symbol.stringValue()));
     }
 
     @Override
@@ -75,21 +77,8 @@ public class IonStructToMapObjectInspector implements MapObjectInspector {
 
         final IonStruct struct = (IonStruct) data;
 
-        // sets the initial size of the map to avoid growing as it's immutable while using the default HashMap load
-        // factor to maintain the same collision performance
-        final int size = (int) Math.ceil(struct.size() / DEFAULT_LOAD_FACTOR);
-
-        final Map<String, IonValue> map = new HashMap<>(size, DEFAULT_LOAD_FACTOR);
-        for (IonValue v : struct) {
-            if (!IonUtil.isIonNull(v)) {
-                map.put(v.getFieldName(), v);
-            } else {
-                // Hive can't handle Ion null
-                map.put(v.getFieldName(), null);
-            }
-        }
-
-        return map;
+        return StreamSupport.stream(struct.spliterator(), false)
+                .collect(HashMap::new, (m,v) -> m.put(v.getFieldName(), IonUtil.handleNull(v)), HashMap::putAll);
     }
 
     @Override
