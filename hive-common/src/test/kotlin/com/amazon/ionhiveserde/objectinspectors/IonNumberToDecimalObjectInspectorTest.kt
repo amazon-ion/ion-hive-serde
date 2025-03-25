bd42c70892cb00ad989870081e322e8b8f9c5cb2
Matthew Pope <popematt@amazon.com>
2025-03-25 12:43:31 -0700

    Adds coercion from IonFloat to HiveDecimal

--- hive-common/src/test/kotlin/com/amazon/ionhiveserde/objectinspectors/IonNumberToDecimalObjectInspectorTest.kt
+++ hive-common/src/test/kotlin/com/amazon/ionhiveserde/objectinspectors/IonNumberToDecimalObjectInspectorTest.kt
@@ -7,6 +7,9 @@ import com.amazon.ion.IonValue
 import com.amazon.ionhiveserde.ION
 import org.apache.hadoop.hive.common.type.HiveDecimal
 import org.apache.hadoop.hive.serde2.io.HiveDecimalWritable
+import org.junit.Test
+import org.junit.jupiter.api.assertThrows
+import java.lang.NumberFormatException
 
 class IonNumberToDecimalObjectInspectorTest
     : AbstractIonPrimitiveJavaObjectInspectorTest<IonValue, HiveDecimalWritable, HiveDecimal>() {
@@ -14,9 +17,18 @@ class IonNumberToDecimalObjectInspectorTest
     override fun validTestCases() = listOf(
             ValidTestCase(ION.newDecimal(1), HiveDecimal.ONE, HiveDecimalWritable(HiveDecimal.ONE)),
             ValidTestCase(ION.newDecimal(0), HiveDecimal.ZERO, HiveDecimalWritable(HiveDecimal.ZERO)),
+            ValidTestCase(ION.newFloat(1), HiveDecimal.ONE, HiveDecimalWritable(HiveDecimal.ONE)),
+            ValidTestCase(ION.newFloat(0), HiveDecimal.ZERO, HiveDecimalWritable(HiveDecimal.ZERO)),
+            ValidTestCase(ION.newInt(1), HiveDecimal.ONE, HiveDecimalWritable(HiveDecimal.ONE)),
             ValidTestCase(ION.newInt(0), HiveDecimal.ZERO, HiveDecimalWritable(HiveDecimal.ZERO)),
-            ValidTestCase(ION.newInt(0), HiveDecimal.ZERO, HiveDecimalWritable(HiveDecimal.ZERO))
     )
+
+    @Test
+    fun `non-numeric floats cannot be converted`() {
+        assertThrows<NumberFormatException> { subject.getPrimitiveJavaObject(ION.newFloat(Double.NaN)) }
+        assertThrows<NumberFormatException> { subject.getPrimitiveJavaObject(ION.newFloat(Double.NEGATIVE_INFINITY)) }
+        assertThrows<NumberFormatException> { subject.getPrimitiveJavaObject(ION.newFloat(Double.POSITIVE_INFINITY)) }
+    }
 
     override val subject = com.amazon.ionhiveserde.objectinspectors.IonNumberToDecimalObjectInspector()
 }
